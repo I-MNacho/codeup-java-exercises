0aac9adbafb3ff158b2fd62791c182df5987e75c
Ignacio Mares <ignacio.mares00@gmail.com>
2021-09-03 16:57:18 -0500

    added more bonus work

--- src/ControlStatementAndLoops.java
+++ src/ControlStatementAndLoops.java
@@ -14,22 +14,84 @@ public class ControlStatementAndLoops {
         //Key differences about Java statements vs javascript
         // no Automatic Semicolon Insertion
         //all variables need a type declaration
+// --------------------------------------------------------------------------
 
         // Conditional Branching:
         // Almost identical to JS.
-        int x = 5;
-        if (x == 5){
-            System.out.println("If you are reading this, x must be 5 since this is running");
-        }
-        System.out.println("On the other hand , this doesn't mean anything because it runs regardless of what x is");
+//        int x = 5;
+//        if (x == 5){
+//            System.out.println("If you are reading this, x must be 5 since this is running");
+//        }
+//        System.out.println("On the other hand , this doesn't mean anything because it runs regardless of what x is");
 
-        int y = 2;
-        y = 3;
-        if (y == 2){
-            System.out.println("I've carried out a conditional test and I can confirm that the statement 'y == 2' is true");
-        }else{
-            System.out.println("The result of my condtional test was false");
-        }
+
+//// --------------------------------------------------------------------------
+//        int y = 2;
+//        y = 3;
+//        if (y == 2){
+//            System.out.println("I've carried out a conditional test and I can confirm that the statement 'y == 2' is true");
+//        }else{
+//            System.out.println("The result of my conditional test was false");
+//        }
 //        System.out.println(y==2);
-}
+
+
+// --------------------------------------------------------------------------
+        //If else statements
+//        short y = -5;
+//        y = 12;
+//        y = 32450;
+//    // y = 33000; //incompatible types: possible lossy conversion from int to short
+//
+//        if (y < 0 ) {
+//            System.out.println("The variable y contains a negative number");
+//        } else if (y >=0 && y < 30000){
+//            System.out.println("The variable y contains a positive number");
+//        } else {
+//            System.out.println("The variable y is dangerously close to the max value for short type variables, which is " + Short.MAX_VALUE);
+//        }
+
+// --------------------------------------------------------------------------
+
+        //Switch statements
+//        String customerChoice = "vanilla";
+//
+//        switch (customerChoice) {
+//            case "vanilla":             //<-- what you are checking for
+//                System.out.println("One vanilla coming right up");//<-- output
+//                break;
+//            case "chocolate":
+//                System.out.println("chocolate coming right up");
+//                break;
+//            case "strawberry":
+//                System.out.println("strawberry coming right up");
+//                break;
+//            default:
+//                System.out.println("We don't have that");
+//                break;
+//        }
+
+// --------------------------------------------------------------------------
+//        Write each program in a separate class
+
+        //BONUS WORK
+//        write a short Java program that tests if a number is positive or negative. Define a few numeric variables and run them through a decision. If the number is positive, output that it is positive, and if it is negative, output that it is negative.
+        int x = 0;
+        if (x > 0){
+            System.out.println("This is a positive number.");
+        }else{
+            System.out.println("Idk bro, might be negative.");
+        }
+
+
+
+
+
+
+        //BONUS WORK
+//        if you finish early and want a greater challenge, write another Java program that takes a hard-coded number, multiplies it by 2, and determines if the result is less than -100, more than 100, or in between, and tells the user
+//        int y = 50;
+//
+//        if (y > )
+    }
 }
