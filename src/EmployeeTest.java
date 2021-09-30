5f74692859b88378833d68709bac477710a8f6b0
Ignacio Mares <ignacio.mares00@gmail.com>
2021-09-30 09:46:06 -0500

    added updated info

--- src/EmployeeTest.java
+++ src/EmployeeTest.java
@@ -1,33 +1,33 @@
-public class EmployeeTest {
-    public static void main(String[] args) {
-        //object construction
-        //part one:
-        //variable
-        //declaration
-        //with a type
-        //declaration
-        Employee one = new Employee();
-        one.name = "one";
-        //object construction part two:
-                    //invoking a constructor
-                    // with the ** new ** keyword
-                    // followed by the name of the class
-                    // and parentheses (parens)
-                    // this creates a new instance of that class
-        Employee two = new Employee();
-        two.name = "two";
-        //object construction part three:
-                //assignment operator =
-                //assigns the newly created object to the reference variable
-
-        Employee three = new Employee();
-        three.name = "three";
-
-        //call newly created object with method
-        one.displayEmployeeInfo();
-        two.displayEmployeeInfo();
-        three.displayEmployeeInfo();
-        System.out.println(Employee.company);
-        System.out.println();
-    }
-}
+//public class EmployeeTest {
+//    public static void main(String[] args) {
+//        //object construction
+//        //part one:
+//        //variable
+//        //declaration
+//        //with a type
+//        //declaration
+//        Employee one = new Employee();
+//        one.name = "one";
+//        //object construction part two:
+//                    //invoking a constructor
+//                    // with the ** new ** keyword
+//                    // followed by the name of the class
+//                    // and parentheses (parens)
+//                    // this creates a new instance of that class
+//        Employee two = new Employee();
+//        two.name = "two";
+//        //object construction part three:
+//                //assignment operator =
+//                //assigns the newly created object to the reference variable
+//
+//        Employee three = new Employee();
+//        three.name = "three";
+//
+//        //call newly created object with method
+//        one.displayEmployeeInfo();
+//        two.displayEmployeeInfo();
+//        three.displayEmployeeInfo();
+//        System.out.println(Employee.company);
+//        System.out.println();
+//    }
+//}
---
+++ src/Marker.java
@@ -0,0 +1,29 @@
+//class Person {
+//    private String name;
+//
+//    public Person(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//}
+//class Employee extends Person {
+//    private String department;
+//
+//    public Employee(String name, String department) {
+//        super(name);
+//        this.department = department;
+//    }
+//
+//    public String getDepartment() {
+//        return department;
+//    }
+//}
+//class CompanyApp {
+//    public static void main(String[] args) {
+//        Employee emp = new Employee("Peter Gibbons", "IT");
+//        System.out.println(emp.getName() + " works in " + emp.getDepartment());
+//    }
+//}
