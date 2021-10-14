53915398e0fc38fbaf92069e3444c0cc18e46e5f
Ignacio Mares <ignacio.mares00@gmail.com>
2021-10-14 09:26:50 -0500

    added a warmup drill

---
+++ src/JavaDrills.java
@@ -0,0 +1,25 @@
+//In `codeup-java-exercises`, create a class `JavaDrills`, with the following:
+//Create a public static method, `flipOuterCase`, that takes in a string and returns the string
+//with the case reversed for the first and last letters. Assume the input will be three or more characters long.
+public class JavaDrills {
+
+//    work in progress
+    public static String flipOuterCase(String input){
+
+
+
+
+
+
+
+        System.out.println(flipOuterCase("cat"));//CaT
+        System.out.println(flipOuterCase("CaT"));//cat
+        System.out.println(flipOuterCase("caT"));//Cat
+        System.out.println(flipOuterCase("cAt"));//CAT
+    }
+
+
+
+
+
+}
